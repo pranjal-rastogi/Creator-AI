2c2c1f6b0b03496e879446d7d4ea26ccc7361c2d
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-11-18 14:25:11 +0530

    Update page.tsx

--- app/dashboard/settings/[[...rest]]/page.tsx
+++ app/dashboard/settings/[[...rest]]/page.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 function Settings() {
   return (
-    <div className='flex items-center justify-center h-full'>
-        <UserProfile/>
+    <div className='flex items-center justify-center min-h-screen px-4'>
+      <div className='w-full sm:max-w-lg'>
+        <UserProfile />
+      </div>
     </div>
   )
 }
