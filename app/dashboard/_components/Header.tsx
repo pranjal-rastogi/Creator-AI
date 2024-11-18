c8b9d49aac5b4239ae0856fffe798c15ff3030ad
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-11-18 14:17:58 +0530

    Update Header.tsx

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -5,17 +5,19 @@ import React from 'react'
 function Header() {
   return (
     <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
-      <div className='flex gap-2 items-center
-       p-2 border rounded-md max-w-lg bg-white'>
-        <Search/>
-        <input type='text' placeholder='Search...'
-        className='outline-none'
+      <div className='flex gap-2 items-center p-2 border max-w-lg bg-white'>
+        <Search />
+        <input
+          type='text'
+          placeholder='Search...'
+          className='outline-none w-full'
         />
       </div>
       <div className='flex gap-5 items-center'>
-        <h2 className='bg-primary p-1 rounded-full text-sm text-white px-2'>
-        🔥 Join Membership just for Rs.99/Month</h2>
-      <UserButton/>
+        <h2 className='bg-primary p-1 text-sm text-white px-2 hidden sm:block'>
+          Get Membership for Rs. 99/Month
+        </h2>
+        <UserButton />
       </div>
     </div>
   )
