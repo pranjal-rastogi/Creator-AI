de4b2f7d88c74bd404299ac25712393f07345ebc
Pranjal Rastogi <126326239+pranjal-rastogi@users.noreply.github.com>
2024-11-18 14:20:16 +0530

    Update Header.tsx

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
-      <div className='flex gap-2 items-center p-2 border max-w-lg bg-white'>
+    <div className='p-5 shadow-sm border-b-2 bg-white flex flex-col sm:flex-row sm:justify-between items-center'>
+      <div className='flex gap-2 items-center p-2 border sm:max-w-lg w-full bg-white'>
         <Search />
         <input
           type='text'
@@ -13,7 +13,7 @@ function Header() {
           className='outline-none w-full'
         />
       </div>
-      <div className='flex gap-5 items-center'>
+      <div className='flex flex-col sm:flex-row gap-5 items-center mt-4 sm:mt-0'>
         <h2 className='bg-primary p-1 text-sm text-white px-2 hidden sm:block'>
           Get Membership for Rs. 99/Month
         </h2>
